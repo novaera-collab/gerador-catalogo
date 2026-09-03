efb2ecadc85e19f26cae1a6aa0769924bcbf98fe
novaera-collab <novaera@unimake.com.br>
2026-09-03 14:16:23 -0300

    Fix typo in set_default_color_theme method

--- app_gestao_encarte.py
+++ app_gestao_encarte.py
@@ -1057,6 +1057,6 @@ class AppPrincipal(ctk.CTk):
 
 if __name__ == "__main__":
     ctk.set_appearance_mode("Dark")
-    ctk.set_defaultcolor_theme("blue")
+    ctk.set_default_color_theme("blue")
     app = AppPrincipal()
     app.mainloop()
